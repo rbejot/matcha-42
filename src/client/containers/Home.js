2ea743aa7b14dadee7e6b9c94273e5bd7bd6fd0f
Rodolphe <rbejot@student.42.fr>
2019-01-03 16:07:11 +0100

    add BrowserRouter front

--- src/client/containers/Home.js
+++ src/client/containers/Home.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Home = ({state, dispatch}) => {
   return (
-    <div>Home test : count : {state.count}</div>
+    <div>
+      Home test : count : {state.count}
+      <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
+    </div>
   );
 }
 
